e4534fcc325f7ae6b9cfdd660dce2f1a5ef925dc
Kelly Littlepage <kelly@onechronos.com>
2022-12-06 15:30:38 -0500

    feat: expose  in top-level

--- src/us/equities/mod.rs
+++ src/us/equities/mod.rs
@@ -1,6 +1,6 @@
 pub mod parser;
 
 pub use parser::{
-    from_any_to_cms, from_any_to_cqs, parse, CMSSuffix, CQSSuffix, NasdaqIntegrated, RootSymbol,
-    Suffix, Symbol, SymbologyError,
+    from_any_to_cms, from_any_to_cqs, from_any_to_nasdaq, parse, CMSSuffix, CQSSuffix,
+    NasdaqIntegrated, RootSymbol, Suffix, Symbol, SymbologyError,
 };
